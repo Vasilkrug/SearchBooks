033c5559fd630f9258bd18a6961d6c4543609cd9
vasilkrug <vkruglovec@gmail.com>
2023-03-20 21:09:10 +0300

    add Header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,34 @@ import './App.css';
 const App = () => {
     return (
         <div className="App">
-            hi
+            <header className={'header'}>
+                <div className="logo">
+                    <p>BookSpace</p>
+                </div>
+                <form className={'search-form'}>
+                    <label>
+                        <input className={'search'} type="text" placeholder={'Search'}/>
+                    </label>
+                    <label>
+                        <select id={'categories'}>
+                            <option value="all">all</option>
+                            <option value="art">art</option>
+                            <option value="biography">biography</option>
+                            <option value="computers">computers</option>
+                            <option value="history">history</option>
+                            <option value="medical">medical</option>
+                            <option value="poetry">poetry</option>
+                        </select>
+                    </label>
+                    <label>
+                        <select id={'sort'}>
+                            <option value="relevance">relevance</option>
+                            <option value="newest">newest</option>
+                        </select>
+                    </label>
+                    <button className={'search-button'}>Search</button>
+                </form>
+            </header>
         </div>
     );
 
