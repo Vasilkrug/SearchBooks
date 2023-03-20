7a8917f8cd90f116c35f44b4ea42d293a3656dd3
vasilkrug <vkruglovec@gmail.com>
2023-03-20 22:17:05 +0300

    add options data for Select

---
+++ src/components/data/data.js
@@ -0,0 +1,13 @@
+export const categoriesOptions = [
+    {value: 'all'},
+    {value: 'art'},
+    {value: 'biography'},
+    {value: 'computers'},
+    {value: 'history'},
+    {value: 'medical'},
+    {value: 'poetry'},
+];
+export const sortOptions = [
+    {value:'relevance'},
+    {value: 'newest'}
+]
