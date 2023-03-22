9ad9892a2870e3e3a57820ea04209355c83b542f
vasilkrug <vkruglovec@gmail.com>
2023-03-22 23:08:26 +0300

    add component BookPreview

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import Header from "./components/header/Header";
 import BooksList from "./components/booksList/BooksList";
 import Loader from "./components/loader/Loader";
 import {useSelector} from "react-redux";
+import {Route, Routes} from "react-router-dom";
+import BookPreview from "./components/bookPreview/BookPreview.";
 
 const App = () => {
     const isLoading = useSelector(state => state.books.isLoading)
-
+    const selectedBook = useSelector(state => state.books.selectedBook)
     return (
         <div className={'App'}>
             <Header/>
-            <BooksList/>
+            <Routes>
+                <Route path={'/'} element={<BooksList/>}/>
+                <Route
+                    path={'/:id'}
+                    element={<BookPreview selectedBook={selectedBook}/>}/>
+            </Routes>
             {isLoading ? <Loader/> : null}
         </div>
     );
---
+++ src/components/bookPreview/BookPreview..jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './BookPreview.css';
+
+const BookPreview = ({selectedBook}) => {
+    const emptyImg = require('../../assets/images/emptyImg.jpg')
+    const {title, description, authors, categories} = selectedBook.volumeInfo;
+    const {thumbnail} = selectedBook.volumeInfo.imageLinks || emptyImg;
+
+    return (
+        <div className={'book-preview'}>
+            <div className={'book-preview-head'}>
+                <p className={'author-preview'}>{authors[0] || 'Без автора'}</p>
+                <p>{title}</p>
+                <span>{!categories ? 'Без категории' : categories[0]}</span>
+            </div>
+            <div className={'book-preview-img'}>
+                <img src={thumbnail || emptyImg} alt=""/>
+            </div>
+            <div className={'book-preview-description'}>
+                <p>{!description ? 'Без описания' : description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default BookPreview;
