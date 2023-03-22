3194dced1d16eddcfe568ebd110692b513f9025c
vasilkrug <vkruglovec@gmail.com>
2023-03-22 15:29:39 +0300

    change api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,10 +3,10 @@ const key = 'AIzaSyAQrcw62zlWuvJo3l6KqepGkD0osFJbYqc';
 export const fetchToApi = async (value, categorySelect, sortSelect) => {
     const url = `https://www.googleapis.com/books/v1/volumes?q=`;
     const orderBy = `orderBy=${sortSelect}`;
-    const category = `subject:${categorySelect}`;
+    const category = `%2Bsubject:${categorySelect}`;
 
     try {
-        const request = await fetch(`${url}${value}%20${category}&${orderBy}&maxResults=30&key=${key}`)
+        const request = await fetch(`${url}${value}${category}&${orderBy}&maxResults=30&key=${key}`)
         return await request.json()
     } catch (e) {
         console.error(e)
